93d31db7de65f0ca906a93b2cc63d9d061a567c5
ermak0va <71447375+ermak0va@users.noreply.github.com>
2020-09-28 20:23:21 +0300

    Add files via upload

--- myrobot/lecture2_7.jl
+++ myrobot/lecture2_7.jl
@@ -2,11 +2,19 @@ function chess(r)
     m=Nord
     i=0
     c=1
+    x=0
+    y=0
+    yy=1
     while isborder(r,West)==false
         i=i+1
         m=HorizonSide(mod(Int(m)+2,4))
         num=paint(r,m)
         move!(r,West)
+        x=x+1
+        if yy==1
+            y=num
+            yy=0
+        end
         if (c==1) && isodd(num)
             i=0
             c=0
@@ -41,8 +49,26 @@ function chess(r)
     end
     m=HorizonSide(mod(Int(m)+2,4))
     paint(r,m)
+    go_conor(r)
+    go_to(r,x,y)
 end
 
+function go_to(r,x,y)
+    for i in 1:x
+        move!(r,Ost)
+    end
+    for j in 1:y
+        move!(r,Nord)
+    end
+end
+function go_conor(r)
+    while isborder(r,West)==false
+        move!(r,West)
+    end
+    while isborder(r,Sud)==false
+        move!(r,Sud)
+    end
+end
 function paint(r,side)
     k=0
     while isborder(r,side)==false
