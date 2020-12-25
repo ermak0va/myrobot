f81d09e986a3162bf7be05fa3ee6151778d9059e
ermak0va <71447375+ermak0va@users.noreply.github.com>
2020-12-25 19:22:30 +0300

    Update lecture4_16.jl

--- myrobot/lecture4_16.jl
+++ myrobot/lecture4_16.jl
@@ -1,12 +1,35 @@
 function mark_field(r)
+    x=0
+    y=0
+    y_all=0
+    while move_if_possible!(r,Sud)==true
+        y=y+1
+    end
+    while move_if_possible!(r,West)==true
+        x=x+1
+    end
     side_ort=Ost
     side_vert=Nord
     while isborder(r,side_ort)==false
         mark_row(r,side_vert)
         move!(r,side_ort)
+        y_all=y_all+1
         side_vert=inverseside(side_vert)
     end
     mark_row(r,side_vert)
+    if side_vert==Ost
+        while move_if_possible!(r,West)==true
+        end
+    end
+    while x>0
+        x=x-1
+        move_if_possible!(r,Ost)
+    end
+    y=y_all-y
+    while y>0
+        y=y-1
+        move_if_possible!(r,Sud)
+    end
 end
 
 
