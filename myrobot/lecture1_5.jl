30269f009a4c4c21120f7b5c835b0e776cb7f85b
ermak0va <71447375+ermak0va@users.noreply.github.com>
2020-12-25 19:15:21 +0300

    Add files via upload

---
+++ myrobot/lecture1_5.jl
@@ -0,0 +1,25 @@
+function mark_angles(r)
+    num_steps=[]
+    x=0
+    y=0
+    while move_if_possible!(r,Sud)==true
+        y=y+1
+    end
+    while move_if_possible!(r,West)==true
+        x=x+1
+    end
+    for side in (Nord, Ost, Sud, West)
+        while isborder(r,side)==false
+            move!(r,side)
+        end
+        putmarker!(r)
+    end
+    while x>0
+        x=x-1
+        move_if_possible!(r,Ost)
+    end
+    while y>0
+        y=y-1
+        move_if_possible!(r,Nord)
+    end
+end
