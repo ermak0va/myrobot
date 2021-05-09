8b3f07a6b80abf3db89559251078d03b89969238
ermak0va <71447375+ermak0va@users.noreply.github.com>
2021-05-10 00:16:36 +0300

    Update dz_4_5.jl

--- julia/prac2/dz_4_5.jl
+++ julia/prac2/dz_4_5.jl
@@ -8,7 +8,7 @@ function calcsort_numzeros(a)
     b=[@view a[:,j] for j in 1:size(a,2)]
     k_s=[length(findall(b[j].== 0)) for j in 1:length(b)]
     b = calcsortkey(b, k_s, collect(0:size(a,1)))
-    return hcat(b...) # из вектора b, содержащего столбцы, снова формируется матрица путем горизонтальной конкатенации
+    return hcat(b...)
 end
 
 """
@@ -18,7 +18,7 @@ calcsortkey(a, key_series, key_values)
 """
 function calcsortkey(a, key_series, key_values)
     vector_vector_indexes = calcsortindexes(key_series, key_values)
-    # vector_vector_indexes - вектор, содержащий ветора
+
     c=similar(a)
     j=1
     for vector_indexes in vector_vector_indexes  
@@ -29,33 +29,16 @@ function calcsortkey(a, key_series, key_values)
     end
     return c
 end
-
-"""
-    calcsortindexes(key_series, key_values)
-
-возвращает vector_vector_indexes
-    пример: 
-        key_values = [0,1,2,3]
-        key_sries = [2,3,0,2,0]
-
-        vector_vector_indexes = calcsortindexes(key_series, key_values)
-    тогда
-        vector_vector_indexes[1] = [3,5]
-        vector_vector_indexes[2] = []
-        vector_vector_indexes[3] = [1,4]
-        vector_vector_indexes[4] = [2]
-"""    
+   
 function calcsortindexes(key_series, key_values) #m+1
 
     vector_vector_indexes=[Int[] for _ in key_values]
-    # - это vector_vector_indexes - вектор соразмерный с key_values, состоящий из пустых векторов типа Vector{Int}  
-    
+
     for i in eachindex(key_series)
         k=indexvalue(key_series[i], key_values)
         push!(vector_vector_indexes[k], i)
     end
-    #для каждого k vector_vector_indexes[k] - содержит индексы key_series, соответствующие значению key_values[k] 
-    
+
     return vector_vector_indexes
 end
 
